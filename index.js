6197fec63e98356a1350659867bc02ae6a66170f
Sumit Sharma <117111687+sumitsharma37712@users.noreply.github.com>
2024-06-06 18:53:10 +0530

    problem

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const connect = require('./Database/config')
 const router = require('./Router')
 const cookieParser = require('cookie-parser')
 
+app.use(cookieParser())
 app.use(express.json())
 app.use(router)
-app.use(cookieParser())
 
 app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`)
